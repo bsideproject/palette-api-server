8b2db58a2f33ec62092f3b2ce83f2fc416c93fef
WooCheol <jikimee64@gmail.com>
2022-08-07 11:44:35 +0900

    Fix: Diary 엔티티 @SQLDelete 속성 수정

--- src/main/java/com/palette/diary/domain/Diary.java
+++ src/main/java/com/palette/diary/domain/Diary.java
@@ -25,7 +25,7 @@ import org.hibernate.annotations.Where;
     @AttributeOverride(name = "id", column = @Column(name = "diary_id")),
 })
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLDelete(sql = "UPDATE diary SET isDel = 1 WHERE id = ?")
+@SQLDelete(sql = "UPDATE diary SET isDeleted = 1 WHERE id = ?")
 public class Diary extends BaseEntity {
 
     @Column(length = 12)
@@ -39,7 +39,7 @@ public class Diary extends BaseEntity {
 
     @Builder.Default
     @ColumnDefault("0")
-    @Column(name = "id_deleted")
+    @Column(name = "is_deleted")
     private Integer isDeleted = 0;
 
 }
