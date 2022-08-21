a8a3b23424fedbf704f3d55a292fb589426f0926
Jeahoon Jung <jeahoonjung@Jeahoons-MacBook-Pro.local>
2022-08-21 20:53:56 +1200

    Refactor: 스키마 파일 구조 변경 (#56)

--- src/main/java/com/palette/color/fetcher/ColorFetcher.java
+++ src/main/java/com/palette/color/fetcher/ColorFetcher.java
@@ -3,10 +3,10 @@ package com.palette.color.fetcher;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.palette.color.domain.Color;
-import com.palette.color.fetcher.dto.ReadColorOutput;
 import com.palette.color.repository.ColorRepository;
+
 import java.util.List;
-import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
@@ -21,18 +21,9 @@ public class ColorFetcher {
 
     private final ColorRepository colorRepository;
 
-    @DgsQuery(field = "color")
-    public List<ReadColorOutput> findColor() {
-        List<Color> colors = colorRepository.findAll(Sort.by(Direction.ASC, "order"));
-        return colors.stream()
-            .map(color -> ReadColorOutput.builder()
-                .id(color.getId())
-                .startCode(color.getStartCode())
-                .endCode(color.getEndCode())
-                .order(color.getOrder())
-                .build()
-            )
-            .collect(Collectors.toList());
+    @DgsQuery(field = "colors")
+    public List<Color> findColors() {
+        return colorRepository.findAll(Sort.by(Direction.ASC, "order"));
     }
 
 }
