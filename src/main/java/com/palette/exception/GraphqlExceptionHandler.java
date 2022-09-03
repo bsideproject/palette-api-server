17bf3353c46f69ff059610ead8aceb41ecebcc97
WooCheol <jikimee64@gmail.com>
2022-09-04 03:29:38 +0900

    Feat: 센트리 설정
    - 파라미터 스토어 등록(업로드 도메인 정보, 센트리 dns)

--- src/main/java/com/palette/exception/GraphqlExceptionHandler.java
+++ src/main/java/com/palette/exception/GraphqlExceptionHandler.java
@@ -7,8 +7,8 @@ import graphql.GraphQLError;
 import graphql.execution.DataFetcherExceptionHandler;
 import graphql.execution.DataFetcherExceptionHandlerParameters;
 import graphql.execution.DataFetcherExceptionHandlerResult;
-import java.util.HashMap;
-import java.util.Map;
+import io.sentry.Sentry;
+import io.sentry.SentryLevel;
 import java.util.concurrent.CompletableFuture;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -30,19 +30,22 @@ public class GraphqlExceptionHandler implements DataFetcherExceptionHandler {
                 httpStatus);
 
             // 필요시 에러 추적을 위한 인자값 추가
-            Map<String, Object> debugInfo = new HashMap<>();
-            debugInfo.put("somefield", "somevalue");
+//            Map<String, Object> debugInfo = new HashMap<>();
+//            debugInfo.put("somefield", "somevalue");
 
             GraphQLError graphqlError = TypedGraphQLError.newInternalErrorBuilder()
                 .errorType(graphqlErrorType.getErrorType())
                 .message(graphqlErrorType.getMessage())
-                .debugInfo(debugInfo)
+                //.debugInfo(debugInfo)
                 .path(handlerParameters.getPath()).build();
 
             DataFetcherExceptionHandlerResult result = DataFetcherExceptionHandlerResult.newResult()
                 .error(graphqlError)
                 .build();
 
+            Sentry.captureMessage(graphqlErrorType.getCode(), SentryLevel.INFO);
+            Sentry.captureMessage(graphqlErrorType.getMessage(), SentryLevel.INFO);
+            Sentry.captureException(exception);
             return CompletableFuture.completedFuture(result);
         } else {
             return DataFetcherExceptionHandler.super.handleException(handlerParameters);
