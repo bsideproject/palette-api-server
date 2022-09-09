b0536a7def84334a206a2b16f31b4eebd8d9a9c1
WooCheol <jikimee64@gmail.com>
2022-09-09 23:42:20 +0900

    Chore: 유레카 클라이언트 추가

--- src/main/java/com/palette/PaletteApplication.java
+++ src/main/java/com/palette/PaletteApplication.java
@@ -2,9 +2,11 @@ package com.palette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableDiscoveryClient
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableAspectJAutoProxy
