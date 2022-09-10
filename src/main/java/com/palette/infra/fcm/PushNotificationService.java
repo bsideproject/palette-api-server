6306db33f1e4b21363aaccf50f3973551a32714e
WooCheol <jikimee64@gmail.com>
2022-09-11 00:04:32 +0900

    Fix: 알림 데이터에 alarmHistoryId 추가
    - 나머지 알림 반영

--- src/main/java/com/palette/infra/fcm/PushNotificationService.java
+++ src/main/java/com/palette/infra/fcm/PushNotificationService.java
@@ -10,9 +10,7 @@ import com.palette.exception.graphql.UserNotFoundExceptionForGraphQL;
 import com.palette.user.domain.User;
 import com.palette.user.repository.UserRepository;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -27,12 +25,9 @@ public class PushNotificationService {
     private FcmService fcmService;
     private AlarmHistoryService alarmHistoryService;
 
-    public void diaryCreated(Diary diary, List<User> users) throws FirebaseMessagingException {
 
-        Map<User, Set<String>> fcmTokens = new HashMap<>();
-        for (User user : users) {
-            fcmTokens.put(user, user.getFcmTokens());
-        }
+    public void diaryCreated(Diary diary, List<User> users) throws FirebaseMessagingException {
+        Map<User, Set<String>> fcmTokens = getFcmTokens(users);
 
         for (User user : fcmTokens.keySet()) {
             Set<String> tokens = fcmTokens.get(user);
@@ -63,42 +58,48 @@ public class PushNotificationService {
 
     public void historyCreated(History history) throws FirebaseMessagingException {
         List<User> users = userRepository.findUsers(history);
-        Set<String> tokens = fcmService.getTokens(users);
-        if (tokens.isEmpty()) {
-            return;
-        }
+        Map<User, Set<String>> fcmTokens = getFcmTokens(users);
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < users.size(); i++) {
-            sb.append(users.get(i).getNickname()).append("님");
-            if (i == users.size()) {
-                sb.append("의");
-            } else {
-                sb.append("과");
+        for (User user : fcmTokens.keySet()) {
+            Set<String> tokens = fcmTokens.get(user);
+            if (tokens.isEmpty()) {
+                return;
             }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < users.size(); i++) {
+                sb.append(users.get(i).getNickname()).append("님");
+                if (i == users.size()) {
+                    sb.append("의");
+                } else {
+                    sb.append("과");
+                }
+            }
+            sb.append(" 교환일기가 시작됐어요.");
+
+            String noteTitle = sb.toString();
+            String noteBody =
+                Duration.between(history.getStartDate(), history.getEndDate()).toDays()
+                    + "일 후 상대방에게 보여주고 싶은 나의 하루를 기록해보세요.";
+
+            Map<String, String> noteData = new HashMap<>();
+            noteData.put("page", "history");
+            noteData.put("diaryId", history.getDiary().getId().toString());
+            noteData.put("historyId", history.getId().toString());
+
+            StringBuilder historyBody = new StringBuilder();
+            historyBody.append(noteTitle);
+            historyBody.append(noteBody);
+
+            AlarmHistory alarmHistory = alarmHistoryService.createAlarmHistory(
+                toEntity(user, historyBody.toString(), "history",
+                    history.getDiary().getId(), history.getId())
+            );
+
+            noteData.put("alarmHistoryId", alarmHistory.getId().toString());
+
+            fcmService.sendNotification(createNote(noteTitle, noteBody, noteData), tokens);
         }
-        sb.append(" 교환일기가 시작됐어요.");
-
-        String noteTitle = sb.toString();
-        String noteBody =
-            Duration.between(history.getStartDate(), history.getEndDate()).toDays()
-                + "일 후 상대방에게 보여주고 싶은 나의 하루를 기록해보세요.";
-
-        Map<String, String> noteData = new HashMap<>();
-        noteData.put("page", "history");
-        noteData.put("diaryId", history.getDiary().getId().toString());
-        noteData.put("historyId", history.getId().toString());
-
-        StringBuilder historyBody = new StringBuilder();
-        historyBody.append(noteTitle);
-        historyBody.append(noteBody);
-
-        alarmHistoryService.createAlarmHistory(
-            toEntities(users, historyBody.toString(), "history",
-                history.getDiary().getId(), history.getId())
-        );
-
-        fcmService.sendNotification(createNote(noteTitle, noteBody, noteData), tokens);
 
     }
 
@@ -106,11 +107,13 @@ public class PushNotificationService {
         History history = page.getHistory();
         Diary diary = history.getDiary();
         List<User> users = userRepository.findUsers(history);
-        User author = users.stream().filter(user -> user.getId().equals(page.getUserId()))
+
+        User author = users.stream()
+            .filter(user -> user.getId().equals(page.getUserId()))
             .findFirst().orElseThrow(UserNotFoundExceptionForGraphQL::new);
         users.remove(author);
 
-        HashSet<String> tokens = fcmService.getTokens(users);
+        Set<String> tokens = fcmService.getTokens(users);
         if (tokens.isEmpty()) {
             return;
         }
@@ -126,10 +129,12 @@ public class PushNotificationService {
         StringBuilder historyBody = new StringBuilder();
         historyBody.append(noteTitle);
 
-        alarmHistoryService.createAlarmHistory(
-            toEntities(users, historyBody.toString(), "history", diary.getId(),
+        AlarmHistory alarmHistory = alarmHistoryService.createAlarmHistory(
+            toEntity(users.get(0), historyBody.toString(), "history", diary.getId(),
                 history.getId())
         );
+
+        noteData.put("alarmHistoryId", alarmHistory.getId().toString());
 
         fcmService.sendNotification(createNote(noteTitle, noteBody, noteData), tokens);
 
@@ -139,28 +144,34 @@ public class PushNotificationService {
         Diary diary = history.getDiary();
         List<User> users = userRepository.findUsers(history);
 
-        Set<String> tokens = fcmService.getTokens(users);
-        if (tokens.isEmpty()) {
-            return;
+        Map<User, Set<String>> fcmTokens = getFcmTokens(users);
+
+        for (User user : fcmTokens.keySet()) {
+            Set<String> tokens = fcmTokens.get(user);
+            if (tokens.isEmpty()) {
+                return;
+            }
+
+            String noteTitle = "일기가 완성됐어요!";
+            String noteBody = diary.getTitle() + " 일기가 완성됐어요.";
+
+            Map<String, String> noteData = new HashMap<>();
+            noteData.put("page", "history");
+            noteData.put("diaryId", diary.getId().toString());
+            noteData.put("historyId", history.getId().toString());
+
+            StringBuilder historyBody = new StringBuilder();
+            historyBody.append(noteTitle);
+
+            AlarmHistory alarmHistory = alarmHistoryService.createAlarmHistory(
+                toEntity(user, historyBody.toString(), "history", diary.getId(),
+                    history.getId())
+            );
+
+            noteData.put("alarmHistoryId", alarmHistory.getId().toString());
+
+            fcmService.sendNotification(createNote(noteTitle, noteBody, noteData), tokens);
         }
-
-        String noteTitle = "일기가 완성됐어요!";
-        String noteBody = diary.getTitle() + " 일기가 완성됐어요.";
-
-        Map<String, String> noteData = new HashMap<>();
-        noteData.put("page", "history");
-        noteData.put("diaryId", diary.getId().toString());
-        noteData.put("historyId", history.getId().toString());
-
-        StringBuilder historyBody = new StringBuilder();
-        historyBody.append(noteTitle);
-
-        alarmHistoryService.createAlarmHistory(
-            toEntities(users, historyBody.toString(), "history", diary.getId(),
-                history.getId())
-        );
-
-        fcmService.sendNotification(createNote(noteTitle, noteBody, noteData), tokens);
 
     }
 
@@ -183,19 +194,12 @@ public class PushNotificationService {
             .build();
     }
 
-    private List<AlarmHistory> toEntities(List<User> users, String body, String movePage,
-        Long diaryId, Long historyId) {
-        List<AlarmHistory> alarmHistories = new ArrayList<>();
+    private Map<User, Set<String>> getFcmTokens(List<User> users) {
+        Map<User, Set<String>> fcmTokens = new HashMap<>();
         for (User user : users) {
-            alarmHistories.add(AlarmHistory.builder()
-                .user(user)
-                .body(body)
-                .movePage(movePage)
-                .diaryId(diaryId)
-                .historyId(historyId)
-                .build());
+            fcmTokens.put(user, user.getFcmTokens());
         }
-        return alarmHistories;
+        return fcmTokens;
     }
 
 }
