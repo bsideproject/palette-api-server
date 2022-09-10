83baa89128d7b9b96b92da96eba8a33509d60275
WooCheol <jikimee64@gmail.com>
2022-09-11 00:52:42 +0900

    Fix: 알림 읽음처리 mutation 요청값 배열로 변경

--- src/main/java/com/palette/alarmhistory/repository/AlarmHistoryRepository.java
+++ src/main/java/com/palette/alarmhistory/repository/AlarmHistoryRepository.java
@@ -1,8 +1,15 @@
 package com.palette.alarmhistory.repository;
 
 import com.palette.alarmhistory.domain.AlarmHistory;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AlarmHistoryRepository extends JpaRepository<AlarmHistory, Long> {
 
+    @Modifying
+    @Query("update AlarmHistory ah set ah.isRead = true where ah.id in :ids")
+    int readAlarmHistories(@Param("ids") List<Long> ids);
 }
