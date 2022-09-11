32a949b65a1efd8ff24096099a00367f3471c363
Jeahoon Jung <jhoon5245@gmail.com>
2022-09-11 22:07:55 +1200

    fix:userRepository

--- src/main/java/com/palette/user/repository/UserRepository.java
+++ src/main/java/com/palette/user/repository/UserRepository.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    @Query(value = "SELECT u FROM User u WHERE u.email = :email")
+    @Query(value = "SELECT * FROM User u WHERE u.email = :email", nativeQuery = true)
     Optional<User> findUsersIncDeletedUserByEmail(@Param("email") String email);
 
     @Query(value = "SELECT u FROM History h " +
