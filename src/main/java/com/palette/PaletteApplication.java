9083bdb399abf8818a28b62c8fb92f9fa4190500
WooCheol <jikimee64@gmail.com>
2022-09-23 00:45:41 +0900

    Chore: main CI / CD 테스트

--- src/main/java/com/palette/PaletteApplication.java
+++ src/main/java/com/palette/PaletteApplication.java
@@ -2,18 +2,12 @@ package com.palette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableDiscoveryClient
-@EnableJpaAuditing
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class PaletteApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PaletteApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PaletteApplication.class, args);
+	}
 
 }
